81efa33cce5c19b3d5600ee637901e21ea52e1aa
wolfecameron <wolfe.cameron@utexas.edu>
2018-12-01 12:45:46 -0600

    created method to calculate the number of teeth for a gear

--- gear.py
+++ gear.py
@@ -1,5 +1,7 @@
 """this file implements a class for a gear that stores all information needed for a gear within a mechanism"""
 
+import numpy as np
+
 class Gear():
 
 	def __init__(self, rad, pos, prev_gear):
@@ -12,6 +14,14 @@ class Gear():
 		self.prev_gear = prev_gear # stored as an index
 		self.next_gears = [] # stored as a list of indices
 		self.ratio = 1.0 # stored as a float
+	
+	def get_num_teeth(self, circular_pitch):
+		"""determines the number of teeth that should be used for
+		the current gear"""
+
+		# calculate number of teeth in the gear and round to nearest int
+		n_teeth = (np.pi*2.0*self.radius)/circular_pitch
+		return round(n_teeth)	
 
 	def __str__(self):
 		"""to string method for gear object"""
